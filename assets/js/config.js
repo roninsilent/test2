5ebeab044ba13fd5212360fdd675d665309b6415
roninsilent <roninsilent@gmail.com>
2023-08-02 17:08:34 +0200

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBQmg7mEH5TE8CEhz6mJwR7gkYfPbHuLeE",
+    authDomain: "jimecoding.firebaseapp.com",
+    databaseURL: "https://jimecoding.firebaseio.com",
+    projectId: "jimecoding",
+    storageBucket: "jimecoding.appspot.com",
+  };firebase.initializeApp(firebaseConfig);
